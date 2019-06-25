b8008e841dbda07603b47ea56dd4729b942efc15
benrybinski <benrybin@gmail.com>
2019-06-25 14:17:53 -0400

    all done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,85 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    Map<String,List<String>> myMap;
+
     public PhoneBook(Map<String, List<String>> map) {
+        myMap=map;
+
     }
 
-    public PhoneBook() {
-        this(null);
+    public PhoneBook()
+    {
+        myMap = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+
+        List temp = new ArrayList<String>();
+        temp.add(phoneNumber);
+        myMap.put(name,temp);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        List temp = new ArrayList<String>();
+        temp.addAll(Arrays.asList(phoneNumbers));
+        myMap.put(name,temp);
+
+
     }
 
     public void remove(String name) {
+        myMap.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+
+        for (Map.Entry<String,List<String>> entry: myMap.entrySet()) {
+           if( entry.getValue().contains(name)){
+               return true;
+           }
+
+        }
+
+
+        return myMap.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+
+
+        return myMap.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+
+        for (Map.Entry<String, List<String>> entry : myMap.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
+                return entry.getKey();
+            }
+        }
         return null;
+
+
+
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List temp = new ArrayList();
+        for (Map.Entry<String, List<String>> entry : myMap.entrySet()) {
+            temp.add(entry.getKey());
+
+            }
+
+        return temp;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return myMap;
     }
 }
